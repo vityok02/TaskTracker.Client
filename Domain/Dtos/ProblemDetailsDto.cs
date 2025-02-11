efa677099cdc3f203c5160559f1366183bf4aa0c
lipkan02 <lipkan02@gmail.com>
2025-02-11 21:28:01 +0200

    add validation model

---
+++ Domain/Dtos/ProblemDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Abstract;
+
+namespace Domain.Dtos;
+
+public record ProblemDetailsDto(
+    string Type,
+    string Title,
+    int Status,
+    string Detail,
+    IEnumerable<Error> Errors);
