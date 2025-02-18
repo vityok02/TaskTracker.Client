54f6f3e09f43f3dff99c93b9142fbdd94deb327d
lipkan02 <lipkan02@gmail.com>
2025-02-18 18:12:49 +0200

    add ApplicationState, update components, improve error handling

---
+++ Client/ApplicationState.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Client;
+
+public class ApplicationState : INotifyPropertyChanged
+{
+    private string? _errorMessage;
+
+    public string? ErrorMessage
+    {
+        get => _errorMessage;
+        set
+        {
+            if (_errorMessage != value)
+            {
+                _errorMessage = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ErrorMessage)));
+            }
+        }
+    }
+
+    public void SetError(string? message)
+    {
+        ErrorMessage = message;
+        OnPropertyChanged(nameof(ErrorMessage));
+    }
+
+    public bool HasError => !string.IsNullOrEmpty(ErrorMessage);
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
