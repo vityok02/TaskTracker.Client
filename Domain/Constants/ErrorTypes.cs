a2b222a0c513ce13f22e55779fa2acc2a125f8e0
lipkan02 <lipkan02@gmail.com>
2025-02-20 11:26:24 +0200

    fix: bug with singleton and errors

--- Domain/Constants/ErrorTypes.cs
+++ Domain/Constants/ErrorTypes.cs
@@ -2,8 +2,8 @@
 
 public class ErrorTypes
 {
-    public readonly string ValidationError = "ValidationError";
-    public readonly string NotFound = "NotFound";
+    public const string ValidationError = "ValidationError";
+    public const string NotFound = "NotFound";
     public const string Unauthorized = "Unauthorized";
     public const string InvalidCredentials = "InvalidCredentials";
     public const string Conflict = "AlreadyExists";
