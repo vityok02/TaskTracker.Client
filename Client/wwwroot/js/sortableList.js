8382c8a93fd1c414bafb9e7ff19970b3587af419
lipkan02 <lipkan02@gmail.com>
2025-04-11 22:32:15 +0300

    refactor

--- Client/wwwroot/js/sortableList.js
+++ Client/wwwroot/js/sortableList.js
@@ -11,23 +11,19 @@
         forceFallback: forceFallback,
         handle: handle || undefined,
         onUpdate: (event) => {
-            // Revert the DOM to match the .NET state
             event.item.remove();
             event.to.insertBefore(event.item, event.to.childNodes[event.oldIndex]);
 
-            // Notify .NET to update its model and re-render
             component.invokeMethodAsync('OnUpdateJS', event.oldDraggableIndex, event.newDraggableIndex);
         },
         onRemove: (event) => {
             if (event.pullMode === 'clone') {
-                // Remove the clone
                 event.clone.remove();
             }
 
             event.item.remove();
             event.from.insertBefore(event.item, event.from.childNodes[event.oldIndex]);
 
-            // Notify .NET to update its model and re-render
             component.invokeMethodAsync('OnRemoveJS', event.oldDraggableIndex, event.newDraggableIndex);
         }
     });
